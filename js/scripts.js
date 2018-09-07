e68b4564d7d40e0d7dc81b8ef4ec7e7319d84190
Joseph Friesen <friesen.josephc@gmail.com>
2018-09-07 14:52:27 -0700

    Add dropdown menus for all options in the top nav bar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  $("#show-memb-link").click(function() {
+
+  $("#show-memb").click(function() {
     $("#drop-memb").toggle();
-  })
+  });
+
+  $("#show-shop").click(function() {
+    $("#drop-shop").toggle();
+  });
+
+  $("#show-prog").click(function() {
+    $("#drop-prog").toggle();
+  });
+
+  $("#show-about").click(function() {
+    $("#drop-about").toggle();
+  });
+
+  $("#show-pdx").click(function() {
+    $("#drop-pdx").toggle();
+  });
+
+  $("#show-madness").click(function() {
+    $("#drop-madness").toggle();
+  });
+
 });
